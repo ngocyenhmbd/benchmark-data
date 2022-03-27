ef088719a0a02fa22e9aa68f7681162777efd08a
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2022-03-27 16:49:48 +0200

    benchmark data for git+file:///home/lasse/Documents/Projects/Tactician/develop/coq-tactician-reinforce#option-demo

--- option-demo/Set-Tactician-Benchmark-10./Injections.v
+++ option-demo/Set-Tactician-Benchmark-10./Injections.v
@@ -1,3 +1,3 @@
 Set Tactician Neural MyBool.
 Set Tactician Neural MyInt 42.
-Set Tactician Neural MyPassword "correct horse battery staple".
+Set Tactician Neural MyPassword "correct horse battery staple2".
