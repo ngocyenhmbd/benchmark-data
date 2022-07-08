bd7729cb3de24986bbb47fc03e1363a1aa1a2105
Fidel I. Schaposnik Massolo <fidel.s@gmail.com>
2022-07-08 13:28:51 -0400

    benchmark data for git+ssh://git@github.com/coq-tactician/coq-tactician-reinforce#b28c6ac7f9a255a2be6a2627c04c83036d040d15

--- b28c6ac7f9a255a2be6a2627c04c83036d040d15/Set-Tactician-Benchmark-60./Injections.v
+++ b28c6ac7f9a255a2be6a2627c04c83036d040d15/Set-Tactician-Benchmark-60./Injections.v
@@ -1,2 +1,2 @@
-Set Tactician Neural Server "127.0.0.1:33333".
+Set Tactician Neural Executable "g2t-server --arch tfgnn --model ~/logs/global_argument_def_qsaving2 --log_level=critical --tf_log_level=critical --tactic_expand_bound=8 --total_expand_bound=10000 --search_expand_bound=100".
 Unset Tactician Neural Truncate.
