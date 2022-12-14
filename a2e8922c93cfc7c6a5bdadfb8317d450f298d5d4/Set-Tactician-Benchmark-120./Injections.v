cb8a9cb3f69fb547cafa0b62734b80d7afbbaac1
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2022-12-14 14:25:07 +0100

    benchmark data for git+ssh://git@github.com/coq-tactician/coq-tactician-reinforce#a2e8922c93cfc7c6a5bdadfb8317d450f298d5d4

--- a2e8922c93cfc7c6a5bdadfb8317d450f298d5d4/Set-Tactician-Benchmark-120./Injections.v
+++ a2e8922c93cfc7c6a5bdadfb8317d450f298d5d4/Set-Tactician-Benchmark-120./Injections.v
@@ -1,2 +1,2 @@
-Set Tactician Neural Executable "pytact-oracle graph /home/lasse/Documents/Projects/Tactician/develop/coq-tactician-stdlib/stdlib+geometry+extlib-lgraph-intermediate-v14-shared".
-Unset Tactician Neural Truncate.
+Set Tactician Neural Executable "pytact-oracle text /home/lasse/Documents/Projects/Tactician/develop/coq-tactician-stdlib/stdlib+geometry+extlib-lgraph-intermediate-v14-shared".
+Set Tactician Neural Textmode.
