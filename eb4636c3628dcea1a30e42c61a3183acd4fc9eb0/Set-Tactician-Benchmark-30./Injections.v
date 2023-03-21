2959fec7213cc5d17164d65976975cf03c3e9653
Jason Rute <Jason.Rute@ibm.com>
2023-03-21 11:53:13 -0400

    benchmark data for git+ssh://git@github.com/coq-tactician/coq-tactician-reinforce#eb4636c3628dcea1a30e42c61a3183acd4fc9eb0

---
+++ eb4636c3628dcea1a30e42c61a3183acd4fc9eb0/Set-Tactician-Benchmark-30./Injections.v
@@ -0,0 +1,2 @@
+Set Tactician Neural Executable "g2t-server --arch hmodel --log_level=info --tf_log_level=critical --tactic_expand_bound=8 --total_expand_bound=10 --search_expand_bound=4 --model /dccstor/mathzero/debug_memory_leak/synthetic_experiments/528_ministdlib_v15_hmodel_main --record /tmp/jasonrute-coq-trace".
+Unset Tactician Neural Truncate.
