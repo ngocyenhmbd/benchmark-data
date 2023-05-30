fbb1bb1f281fc0f5ea34ce63e9745501e1fd67c3
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2023-05-30 15:53:39 +0200

    benchmark data for git+ssh://git@github.com/coq-tactician/coq-tactician-reinforce#35f6f82484723c591a67b00defb7999c03d866f3

--- 35f6f82484723c591a67b00defb7999c03d866f3/Set-Tactician-Benchmark-1./Injections.v
+++ 35f6f82484723c591a67b00defb7999c03d866f3/Set-Tactician-Benchmark-1./Injections.v
@@ -1,1 +1,1 @@
-Set Tactician Neural Executable "pytact-oracle graph /home/lasse/Documents/Projects/Tactician/develop/datasets/v15-stdlib-coq8.11/dataset"
+Set Tactician Neural Executable "pytact-oracle graph /home/lasse/Documents/Projects/Tactician/develop/datasets/v15-stdlib-coq8.11/dataset".
